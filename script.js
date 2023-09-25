dd1467439599ef54f79718ab3a15a22c7651d67a
temmytifs <temmytifs@gmail.com>
2023-09-25 09:05:13 +0100

    new commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 function getwheather() {
   let city = document.getElementById("cityInput").value;
   let tifeApiKey = "748757f44decbdd3da72168d9b197a08";
-  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tifeApiKey}`;
+  let apikURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tifeApiKey}`;
 
-  fetch(queryURL)
+  fetch(apikURL)
       .then((response) => {
           if (!response.ok) {
               throw new Error('network is not avalible');
